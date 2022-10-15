6948f079b360f07160b9384305ffe92c07b2b4d5
ahnsv <ahnsv@bc.edu>
2022-10-15 17:59:56 +0900

    refactor(main): update kafka broker url

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	topic := "test-topic"
 	partition := 0
 
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka.muskat-ahnsv.svc.cluster.local:9092", topic, partition)
 	if err != nil {
 		log.Fatal("failed to dial leader:", err)
 	}
